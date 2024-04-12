23ab298ad07ea2ac7d08df380faeb839ccfb4b77
jeetenmndl <jeetenmndl96@gmail.com>
2024-04-12 17:57:51 +0545

    top description done

--- src/app/room/page.jsx
+++ src/app/room/page.jsx
@@ -6,14 +6,26 @@ const page = () => {
   return (
     
     <main className='px-20 py-8 flex gap-16 justify-between'>
+
+      {/* left section */}
       <div className=' w-4/6'>
-        <CarouselImage />
+
         <CarouselImage />
 
+        <div className='mt-12 mb-4'>
+          <h1 className='text-3xl font-semibold mb-2'>Flat for rent at Peepal Chowk, Biratnagar</h1>
+          <p>Property Owner: <span className='font-medium'>Jeeten Mandal</span></p>
+        <hr className='my-4' />
+        <p className='leading-8'>Welcome to your new home! This charming flat offers comfortable living with its two cozy bedrooms, perfect for a small family or roommates. The bright and airy hall provides a welcoming space for relaxation and entertainment. Cook up delicious meals in the fully equipped kitchen, complete with modern appliances. All rooms come fully furnished, ensuring convenience from day one. Located in a peaceful neighborhood, youll enjoy tranquility while being close to amenities and transport links. Dont miss out on this wonderful opportunity to make this flat your own!</p>
+        </div>
+
       </div>
+
+      {/* right section card  */}
       <div className='w-2/6'>
         <BookingCard />
       </div>
+
     </main>
 
   )
--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -3,15 +3,20 @@ import Logo from '@/../public/logo.png'
 import Image from 'next/image'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
 import { Button } from './ui/button'
+import {Check} from "lucide-react"
+import {X} from "lucide-react"
+
 
 const BookingCard = () => {
   return (
     <div className='rounded-lg sticky top-8 flex justify-end'>
       <Card className="w-full">
+
         <CardHeader>
           <CardTitle>Peepal Chowk, Biratnagar</CardTitle>
           <CardDescription>Near Arniko Campus</CardDescription>
         </CardHeader>
+
         <CardContent>
           
           <div className='flex flex-col py-4'>
@@ -23,11 +28,25 @@ const BookingCard = () => {
             <tbody>
             <tr><td>Stay Type</td><td className=' text-right'>Flat</td></tr>
             <tr><td>Bedroom</td><td className=' text-right'>2</td></tr>
+            <tr><td>Hall</td><td className=' text-right'>1</td></tr>
             <tr><td>Kitchen</td><td className=' text-right'>1</td></tr>
             <tr><td>Toilet/Bathroom</td><td className=' text-right'>Seperate</td></tr>
             </tbody>
           </table>
+
+          <div className='flex gap-4 my-4'>
+            <div className='flex justify-between items-center w-1/2 py-1.5 px-4 rounded bg-gray-200'>
+              <p>Electricity</p>
+              <Check size={20} strokeWidth={3} className='text-green-500' />
+            </div>
+            <div className='flex justify-between items-center w-1/2 py-1.5 px-4 rounded bg-gray-200'>
+              <p>Water Supply</p>
+              <X size={20} strokeWidth={3} className='text-red-500' />
+            </div>
+          </div>
+
         </CardContent>
+        
         <CardFooter>
           <div className='flex flex-col w-full'>
             <Button className="w-full bg-main">Book Now</Button>
