4332ed094ad05d627937818e0147ddeb4f102de1
jeetenmndl <jeetenmndl96@gmail.com>
2024-04-20 18:39:26 +0545

    room page responsive

--- src/app/room/page.jsx
+++ src/app/room/page.jsx
@@ -13,23 +13,23 @@ const page = () => {
   return (
     <>
     
-    <main className='px-20 pt-8 flex gap-16 justify-between'>
+    <main className='md:px-20 md:pt-8 md:flex gap-16 md:justify-between'>
 
       {/* left section */}
-      <div className=' w-4/6'>
+      <div className=' md:w-4/6'>
 
         <CarouselImage />
 
         {/* description  */}
-        <section className='mt-12 mb-4'>
-          <h1 className='text-3xl font-semibold mb-2'>Flat for rent at Peepal Chowk, Biratnagar</h1>
+        <section className='mx-4 md:mx-0 mt-4 md:mt-12 mb-4'>
+          <h1 className='text-2xl md:text-3xl font-semibold mb-2'>Flat for rent at Peepal Chowk, Biratnagar</h1>
           <p>Property Owner: <span className='font-medium'>Jeeten Mandal</span></p>
         <hr className='my-4' />
         <p className='leading-8'>Welcome to your new home! This charming flat offers comfortable living with its two cozy bedrooms, perfect for a small family or roommates. The bright and airy hall provides a welcoming space for relaxation and entertainment. Cook up delicious meals in the fully equipped kitchen, complete with modern appliances. All rooms come fully furnished, ensuring convenience from day one. Located in a peaceful neighborhood, youll enjoy tranquility while being close to amenities and transport links. Dont miss out on this wonderful opportunity to make this flat your own!</p>
         </section>
 
         {/* rooms info and features  */}
-        <section className="my-16 grid grid-cols-2 gap-8">
+        <section className="mx-4 md:mx-0 my-16 grid grid-cols-1 md:grid-cols-2 gap-8">
 
           <Card className="w-full">
 
@@ -119,9 +119,9 @@ const page = () => {
         <hr />
 
         {/* things to know  */}
-        <section className="my-16 ">
+        <section className="md:my-16 my-8 mx-4 md:mx-0">
           <h3 className='text-2xl font-semibold'>Things to Know</h3>
-          <article className='grid grid-cols-2  mt-4 mx-4'>
+          <article className='grid grid-cols-1 md:grid-cols-2  mt-4 mx-4'>
             <ul className=' list-disc [&>li]:py-2'>
               <li>Arrive before 10 PM</li>
               <li>No loud music at night</li>
@@ -129,7 +129,7 @@ const page = () => {
               <li>Rent to be paid on time</li>
             </ul>
 
-            <blockquote className="border-l-2 pl-6 italic">
+            <blockquote className="md:border-l-2 md:pl-6 italic">
             No pets allowed, smoking strictly prohibited indoors, rent due by the first of each month, and guests limited to overnight stays with prior notice. Maintenance requests should be reported promptly. These guidelines ensure a harmonious living environment for all tenants.
             </blockquote>
           </article>          
@@ -141,16 +141,16 @@ const page = () => {
       </div>
 
       {/* right section card  */}
-      <div className='w-2/6'>
+      <div className='md:w-2/6 mx-4 md:mx-0'>
         <BookingCard />
       </div>
 
     </main>
 
 
-    <section className="mx-20 my-16 ">
+    <section className=" mx-4 md:mx-20 my-16 ">
       <h2 className='text-3xl font-semibold mb-6'>Nearby Locations</h2>
-      <article className="grid grid-cols-4 gap-x-6 gap-y-8">
+      <article className="grid grid-cols-1 md:grid-cols-4 gap-x-6 gap-y-8">
       {
         [1,2,3,4].map((item)=>{
           return(
--- src/components/CarouselImage.jsx
+++ src/components/CarouselImage.jsx
@@ -16,7 +16,7 @@ export default function CarouselImage() {
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
+            <div className="md:p-1">
               <Card>
                 <CardContent className="flex w-full aspect-video items-center justify-center p-0 bg-light">
                   <Image src={Img1} alt="room" />
@@ -26,8 +26,8 @@ export default function CarouselImage() {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className=" translate-x-20" />
-      <CarouselNext className="-translate-x-20" />
+      <CarouselPrevious className="translate-x-16 md:translate-x-20" />
+      <CarouselNext className="-translate-x-16 md:-translate-x-20" />
     </Carousel>
   )
 }
